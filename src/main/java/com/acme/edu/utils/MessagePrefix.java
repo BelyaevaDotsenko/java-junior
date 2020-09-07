c35ac606377300c1063ca5dad59dfeb51c47e06a
DotsenkoMax <raxfaxmax@gmail.com>
2020-09-07 17:34:06 +0300

    OOP adding

---
+++ src/main/java/com/acme/edu/utils/MessagePrefix.java
@@ -0,0 +1,10 @@
+package com.acme.edu.utils;
+
+public final class MessagePrefix {
+    public static final String PRIMITIVE_PREFIX = "primitive: ";
+    public static final String STRING_PREFIX = "string: ";
+    public static final String CHAR_PREFIX = "char: ";
+    public static final String REFERENCE_PREFIX = "reference: ";
+    public static final String PRIMITIVE_ARRAY_PREFIX = "primitives array: ";
+    public static final String PRIMITIVE_MATRIX_PREFIX = "primitives matrix: ";
+}
